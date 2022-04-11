ee8ea490d7b868a0ef191eb6d295aa4bcdad1cbf
Aleksa Bubanja <aleksa3b@gmail.com>
2022-04-11 21:29:28 +0200

    alpaca parts added

--- src/component/Accessories.jsx
+++ src/component/Accessories.jsx
@@ -1,100 +1,43 @@
 import { useState } from "react";
+import { alpacaConfig } from "../alpacaConfig";
 
 function Accessories() {
   const [title, setTitle] = useState("Backgrounds");
-  const [active, setActive] = useState("Backgrounds");
+  const [isActive, setIsActive] = useState(false);
 
   return (
     <section className="accessories">
       <div>
         <h1>Accessorize your Alpaca</h1>
         <div className="buttons">
-          <button
-            className={active === "Backgrounds" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Backgrounds
-          </button>
-          <button
-            className={active === "Ears" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Ears
-          </button>
-          <button
-            className={active === "Eyes" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Eyes
-          </button>
-          <button
-            className={active === "Hair" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Hair
-          </button>
-          <button
-            className={active === "Leg" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Leg
-          </button>
-          <button
-            className={active === "Mouth" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Mouth
-          </button>
-          <button
-            className={active === "Neck" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Neck
-          </button>
-          <button
-            className={active === "Accessories" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Accessories
-          </button>
-          <button
-            className={active === "Nose" ? "active" : ""}
-            onClick={(e) => {
-              setTitle(e.target.textContent);
-              setActive(e.target.textContent);
-            }}
-          >
-            Nose
-          </button>
+          {alpacaConfig.map((button) => {
+            return (
+              <button
+                className={isActive === button.id ? "active" : ""}
+                key={button.id}
+                onClick={(e) => {
+                  setTitle(e.target.textContent);
+                  setIsActive(button.id);
+                }}
+              >
+                {button.label}
+              </button>
+            );
+          })}
         </div>
       </div>
       <div className="underline"></div>
       <div>
         <h1>{title}</h1>
+        <div className="buttons parts">
+          {alpacaConfig.map((item) => {
+            if (item.label === title) {
+              return item.items.map((part) => {
+                return <button key={part.id}>{part.label}</button>;
+              });
+            }
+          })}
+        </div>
       </div>
     </section>
   );
--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Image() {
   return (
     <section className="image">
