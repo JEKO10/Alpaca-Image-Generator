2d3902bb1f8567f5efb3ba29802bc10bf46a7e02
Aleksa Bubanja <aleksa3b@gmail.com>
2022-04-12 20:36:08 +0200

    default image

--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -1,13 +1,24 @@
 import { alpacaConfig } from "../alpacaConfig";
 
 function Image() {
+  const base = process.env.PUBLIC_URL;
+
   return (
     <section className="image">
       <div className="content">
+        <img src={base + alpacaConfig[0].items[0].src} alt="BG" id="bg" />
+        <img src={base + alpacaConfig[1].items[0].src} alt="ears" id="ears" />
+        <img src={base + alpacaConfig[2].items[0].src} alt="eyes" id="eyes" />
+        <img src={base + alpacaConfig[3].items[0].src} alt="hair" id="hair" />
+        <img src={base + alpacaConfig[4].items[0].src} alt="leg" id="leg" />
+        <img src={base + alpacaConfig[5].items[0].src} alt="mouth" id="mouth" />
+        <img src={base + alpacaConfig[6].items[0].src} alt="neck" id="neck" />
         <img
-          src={process.env.PUBLIC_URL + alpacaConfig[0].items[8].src}
-          alt="Ears"
+          src={base + alpacaConfig[7].items[0].src}
+          alt="accessories"
+          id="accessories"
         />
+        <img src={base + alpacaConfig[8].items[0].src} alt="nose" id="nose" />
       </div>
       <button id="download">Download</button>
     </section>
